201491bcff5abdf0440af5e99c76e29f4875713e
AjaX-08 <majaysakthishankar@gmail.com>
2024-08-04 12:08:47 +0530

    solving searching algorithm problems

---
+++ h_searching/searchInsertPosition.java
@@ -0,0 +1,26 @@
+package com.dsa_bootcamp_assignments.h_searching;
+//https://leetcode.com/problems/search-insert-position/
+public class searchInsertPosition {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int target = 6;
+        System.out.println(searchPosition(nums, target));
+    }
+
+    static int searchPosition(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
