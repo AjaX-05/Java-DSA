559ac5f30052225348d6eec2ff7bc2b9b2a5f4af
AjaX-08 <majaysakthishankar@gmail.com>
2024-08-17 01:42:12 +0530

    Cyclic Sort

--- l_sortingAlgorithms/d_CyclicSort.java
+++ l_sortingAlgorithms/d_CyclicSort.java
@@ -4,23 +4,40 @@ import java.util.Arrays;
 
 public class d_CyclicSort {
     public static void main(String[] args) {
-        int[] nums = {3, 5, 2, 1, 4};
+        int[] nums = {3, 1, 0, 2};
         cyclic(nums);
-
         System.out.println(Arrays.toString(nums));
     }
 
-    static void cyclic(int[] nums) {
 
+    //    Cyclic Sort 0 - N
+    static void cyclic(int[] nums) {
         int start = 0;
         while (start < nums.length) {
-            if (nums[start] != start + 1) {
+            if (nums[start] != start) {
                 int temp = nums[start];
-                nums[start] = nums[temp - 1];
-                nums[temp - 1] = temp;
+                nums[start] = nums[temp];
+                nums[temp] = temp;
             } else {
                 start++;
             }
         }
     }
+
+
+//    Cyclic Sort 1 - N
+//    static void cyclic(int[] nums) {
+//        int start = 0;
+//        while (start < nums.length) {
+//            if (nums[start] != start + 1) {
+//                int temp = nums[start];
+//                nums[start] = nums[temp - 1];
+//                nums[temp - 1] = temp;
+//            } else {
+//                start++;
+//            }
+//        }
+//    }
+
+
 }
