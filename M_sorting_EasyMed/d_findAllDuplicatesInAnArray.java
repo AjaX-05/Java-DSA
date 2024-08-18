cad208373f35fb8fb850ed7e0acd0c2d00690630
AjaX-08 <majaysakthishankar@gmail.com>
2024-08-18 22:38:12 +0530

    Solving Couple of Leetcode problems on sorting

---
+++ M_sorting_EasyMed/d_findAllDuplicatesInAnArray.java
@@ -0,0 +1,41 @@
+package com.dsa_bootcamp_assignments.M_sorting_EasyMed;
+
+import java.util.*;
+
+public class d_findAllDuplicatesInAnArray {
+
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+
+        ArrayList<Integer> duplicatesList = sort(nums);
+        System.out.println(duplicatesList);
+    }
+
+    static ArrayList<Integer> sort(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correctIndex = nums[i] - 1;
+            if (nums[i] != nums[correctIndex]) {
+                swap(nums, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+
+        ArrayList<Integer> duplicatesList = new ArrayList<>();
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+//                System.out.println(j + 1);
+                duplicatesList.add(nums[j]);
+            }
+        }
+
+        return duplicatesList;
+    }
+
+    static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
---
+++ M_sorting_EasyMed/e_setMismatch.java
@@ -0,0 +1,37 @@
+package com.dsa_bootcamp_assignments.M_sorting_EasyMed;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class e_setMismatch {
+    public static void main(String[] args) {
+        int[] nums = {1, 1};
+        int[] mismatch = sort(nums);
+        System.out.println(Arrays.toString(mismatch));
+    }
+
+    static int[] sort(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correctIndex = nums[i] - 1;
+            if (nums[i] != nums[correctIndex]) {
+                swap(nums, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                return new int[]{nums[j], j + 1};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
