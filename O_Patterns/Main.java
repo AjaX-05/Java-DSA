6565125137301b2aefbdb38d396787232dbb86f4
AjaX-08 <majaysakthishankar@gmail.com>
2024-08-27 14:21:55 -0500

    solving more patterns

--- O_Patterns/Main.java
+++ O_Patterns/Main.java
@@ -2,7 +2,7 @@ package com.dsa_bootcamp_assignments.O_Patterns;
 
 public class Main {
     public static void main(String[] args) {
-        pattern9(5);
+        pattern12(5);
     }
 
     static void pattern1(int n) {
@@ -105,6 +105,44 @@ public class Main {
             }
             for (int i = n * 2 - row * 2; i > 1; i--) {
                 System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern10(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n - row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern11(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < n - row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern12(int n) {
+        for (int row = 1; row <= n * 2 ; row++) {
+            int shade = row > n ? 2 * n - row : row -1;
+            for (int col = 0; col < shade; col++) {
+                System.out.print(" ");
+            }
+            int totalColsInRow = row > n ? row - n : n - row + 1;
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
