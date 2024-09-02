a0ab960b21186f3977264f5cd3509d5c27b4371b
AjaX-05 <majaysakthishankar@gmail.com>
2024-09-02 01:20:26 -0500

    Binary Search Recursion

---
+++ P_Recursion/e_BinarySearchRecursion.java
@@ -0,0 +1,27 @@
+package com.dsa_bootcamp_assignments.P_Recursion;
+
+public class e_BinarySearchRecursion {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 16, 18, 20, 24, 33, 45, 56, 79, 80, 92, 95, 99, 102};
+        int target = 33;
+//        System.out.println(nums.length);
+
+        int res = BS(nums, target, 0, nums.length - 1);
+        System.out.println(res);
+    }
+
+    static int BS(int[] nums, int target, int s, int e) {
+        if (s > e) {
+            return -1;
+        }
+        int m = s + (e - s) / 2;
+        if (nums[m] == target) {
+            return m;
+        }
+        if (target > nums[m]) {
+            return BS(nums, target, m + 1, e);
+        }
+        return BS(nums, target, s, m - 1);
+
+    }
+}
